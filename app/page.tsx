533e883de75e38cfb1d57afdb381fcf573a6704d
4rgc <bohdan.andrii@gmail.com>
2023-04-09 11:27:50 -0400

    refactor: move common layout to layout.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,8 @@
-import Image from "next/image";
-import { Poppins, Fira_Mono } from "next/font/google";
-
-const firaMono = Fira_Mono({
-  weight: ["400", "500", "700"],
-  subsets: ["latin"],
-});
-const poppins = Poppins({
-  weight: "400",
-  subsets: ["latin"],
-});
-
 export default function Home() {
   return (
-    <div className="page bg-lightBg dark:bg-darkBg">
-      <main className="container flex flex-row">
-        <Image
-          className="flex-1 hidden laptop:block"
-          src="left-shapes-l.svg"
-          alt="shapes left"
-          width="378"
-          height="762"
-        />
-        <nav className="flex py-6 px-5 space-y-3 flex-col">
-          <Image
-            src="logo-md-l.svg"
-            alt="logo"
-            width="0"
-            height="0"
-            className="w-11 h-11 tablet:w-16 tablet:h-16"
-          />
-          <div className="flex tablet:flex-col laptop:flex-col space-x-4 items-center">
-            <span
-              className={`${firaMono.className} text-lightSecondary dark:text-darkSecondary underline`}
-            >
-              home
-            </span>
-            <span
-              className={`${firaMono.className} text-lightSecondary dark:text-darkSecondary`}
-            >
-              experience
-            </span>
-            <span
-              className={`${firaMono.className} text-lightSecondary dark:text-darkSecondary`}
-            >
-              projects
-            </span>
-            <span
-              className={`${firaMono.className} text-lightSecondary dark:text-darkSecondary`}
-            >
-              contact
-            </span>
-          </div>
-        </nav>
-        <Image
-          className="flex-1 hidden laptop:block"
-          src="right-shapes-l.svg"
-          alt="shapes right"
-          height="378"
-          width="762"
-        />
-      </main>
-    </div>
+    <>
+      <h1 className="font-mono text-3xl">Hi, I&apos;m Andrii.</h1>
+      <p>I’m an experienced <b>Full Stack Developer</b> building scalable web applications for  <b> 50K+ artists </b> @ Autodesk.</p>
+    </>
   );
 }
