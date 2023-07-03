589d43bc02d9a152a43914443d9990a85924dbc9
4rgc <bohdan.andrii@gmail.com>
2023-07-03 14:41:41 -0400

    feat(experience): add header

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,6 +1,17 @@
 const ExperiencePage = () => {
-  return;
-  <div className="grow"></div>;
+  return (
+    <div className="grow">
+      <div className="flex flex-col space-y-3 pr-7">
+        <h1 className="font-mono text-4xl font-medium">My experience</h1>
+        <p className="text-lg">
+          In my fresh career I have worked in multiple <b>full-stack</b> teams,
+          delivering on complex projects, development and testing infrastructure
+          upgrades for <b>modern web applications</b> with <b>&gt;50K&nbsp;</b>
+          active users.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default ExperiencePage;
